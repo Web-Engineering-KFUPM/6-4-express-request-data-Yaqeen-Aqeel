36e53299696ddcac8dcc62962ec55a2f8614e422
YAQIN SHAWKAN <s202255400@kfupm.edu.sa>
2026-04-08 12:58:26 +0300

    TODO 1

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -105,10 +105,11 @@ LAB SETUP INSTRUCTIONS
  */
 
 //import express
-
+import express from "express";
 
 // create express app instance to create web server
-
+const app = express();
+app.listen(3000, ()=> console.log("API running at http://localhost:3000"));
 
 
 // Query params: /echo?name=Ali&age=22
